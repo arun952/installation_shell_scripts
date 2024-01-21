b0b7fe97dd742cf4efb39f002a14933d6c3b5cff
arun952 <71364238+arun952@users.noreply.github.com>
2024-01-21 14:27:27 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -9,3 +9,7 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get update
 sudo apt-get install jenkins
+
+
+#####To know the initial admin password#####
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
